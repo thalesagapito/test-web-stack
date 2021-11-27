7d71e85631cfb17bf29f2619ae280683be3fc394
Thales Agapito <thsousa101@gmail.com>
2021-11-27 19:19:20 -0300

    add loading prop to base button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
-import UsersList from './components/users/UsersList.vue'
+import { ref } from 'vue'
+import BaseInput from './components/base/BaseInput.vue'
+import UserCardsGrid from './components/users/UserCardsGrid.vue'
+
+const textSearch = ref()
 </script>
 
 <template>
-  <Suspense>
-    <UsersList />
-    <template #fallback>
-      loading...
-    </template>
-  </Suspense>
+  <div class="users-list">
+    <h1>Users list</h1>
+    <BaseInput
+      v-model="textSearch"
+      placeholder="Search..."
+      class="text-search-input"
+    />
+
+    <UserCardsGrid :text-search="textSearch" />
+  </div>
 </template>
+
+<style lang="postcss">
+#app {
+  @apply antialiased flex flex-col container p-8 space-y-6;
+}
+</style>
+
+<style scoped lang="postcss">
+.users-list {
+  @apply grid items-center gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4;
+  h1 {
+    @apply leading-none;
+  }
+  .text-search-input {
+    grid-column: -2;
+  }
+}
+</style>
--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -3,25 +3,29 @@ withDefaults(
   defineProps<{
     label: string
     type: 'primary' | 'secondary'
+    loading: boolean
   }>(),
   {
     label: '',
+    loading: false,
     type: 'primary',
   },
 )
 </script>
 
 <template>
-  <button :class="['base-button', type]">
+  <button :class="{'base-button': true, [type]: true, loading }">
     {{ label }}
   </button>
 </template>
 
 <style scoped lang="postcss">
 .base-button {
-  @apply flex justify-center items-center text-center px-16 py-6 rounded-lg
-  text-xl uppercase border-4 border-black border-opacity-10 font-semibold transition
-  hover:border-opacity-40 focus:border-opacity-50 disabled:text-opacity-50;
+  @apply flex justify-center items-center text-center
+  px-12 py-5 rounded-lg text-xl uppercase border-4
+  border-black border-opacity-10 font-semibold transition
+  hover:border-opacity-40 focus:border-opacity-50
+  disabled:border-opacity-10 disabled:text-opacity-50 disabled:cursor-not-allowed;
 
   &.primary {
     @apply bg-white;
@@ -29,6 +33,10 @@ withDefaults(
 
   &.secondary {
     @apply bg-transparent;
+  }
+
+  &.loading {
+    @apply cursor-wait disabled:cursor-wait;
   }
 }
 </style>
