d30cd232df09b2459698b1476497ba51f490aa8e
Thales Agapito <thsousa101@gmail.com>
2021-11-27 10:57:46 -0300

    update api default auth to API_KEY

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
 import App from './App.vue'
+
+Amplify.configure(awsconfig)
 
 createApp(App).mount('#app')
