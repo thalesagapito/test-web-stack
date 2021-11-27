959e399bb52dc64024b5b59eb428492fc98afef1
Thales Agapito <thsousa101@gmail.com>
2021-11-27 19:19:37 -0300

    add base input component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: string
+    placeholder: string
+    label?: string
+  }>(),
+  {
+    modelValue: '',
+    placeholder: undefined,
+    label: undefined,
+  },
+)
+
+const writableModelValue = useVModel(props, 'modelValue')
+</script>
+
+<template>
+  <div class="base-input">
+    <span v-if="label">
+      {{ label }}
+    </span>
+    <input
+      v-model="writableModelValue"
+      :placeholder="placeholder"
+      :aria-label="label"
+      type="text"
+    >
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.base-input {
+  @apply flex flex-col items-start;
+
+  span {
+    @apply font-semibold;
+    font: 1.125rem;
+  }
+
+  input {
+    @apply rounded-lg placeholder-black placeholder-opacity-40
+    border-black text-xl transition font-light py-3 px-4 w-full
+    outline-none ring-0 border-opacity-10 bg-white focus:border-opacity-40;
+  }
+}
+</style>
