67ade641b01cbbc3265a0c19b9fe1dac06244c76
Thales Agapito <thsousa101@gmail.com>
2021-11-27 14:23:06 -0300

    add empty base components

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    label: string
+    type: 'primary' | 'secondary'
+  }>(),
+  {
+    label: '',
+    type: 'primary',
+  },
+)
+</script>
+
+<template>
+  <button :class="['base-button', type]">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="postcss">
+.base-button {
+  @apply flex justify-center items-center text-center px-6 py-2 rounded
+  uppercase border-2 border-gray-300 font-semibold
+  hover:border-gray-500 focus:border-gray-600 disabled:border-gray-200;
+
+  &.primary {
+    @apply bg-white;
+  }
+
+  &.secondary {
+    @apply bg-gray-200;
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Source Sans Pro', 'sans-serif'],
+    },
+    fontSize: {
+      default: '16px',
+      lg: '1.3125rem',
+      xl: '3rem',
+    },
+    fontWeight: {
+      light: 300,
+      semibold: 600,
+    },
     extend: {
       container: {
         center: true,
@@ -9,7 +21,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderColor: ['disabled'],
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
