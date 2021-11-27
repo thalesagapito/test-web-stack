64c11e72439b73f9a967cad8e17849ae91748f69
Thales Agapito <thsousa101@gmail.com>
2021-11-27 14:08:45 -0300

    add tailwind packages and config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports'
 import App from './App.vue'
+import './index.css'
 
 Amplify.configure(awsconfig)
 
