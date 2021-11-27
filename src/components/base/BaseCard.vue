ad143011e4f6d7436c3f25544812ed6332c1c2c2
Thales Agapito <thsousa101@gmail.com>
2021-11-27 17:13:14 -0300

    add base card component

---
+++ src/components/base/BaseCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="base-card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.base-card {
+  @apply bg-white rounded-lg transition hover:shadow-xl
+}
+</style>
