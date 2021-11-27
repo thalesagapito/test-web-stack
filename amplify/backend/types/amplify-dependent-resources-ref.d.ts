55e2d69d9cd52063df164954e1056e9888c76b02
Thales Agapito <thsousa101@gmail.com>
2021-11-27 00:55:11 -0300

    add initial amplify project structure

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "superformula": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
