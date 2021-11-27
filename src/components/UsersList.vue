4aef41b22ed1e7b7f73c314e339de4772af6767c
Thales Agapito <thsousa101@gmail.com>
2021-11-27 11:00:06 -0300

    remove unused hello world component and add initial users list

---
+++ src/components/UsersList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { API } from 'aws-amplify'
+import { listUsers } from '../graphql/queries'
+
+const users = await API.graphql({ query: listUsers })
+</script>
+
+<template>
+  <pre>
+    {{ users }}
+  </pre>
+</template>
+
+<style scoped lang="postcss">
+</style>
