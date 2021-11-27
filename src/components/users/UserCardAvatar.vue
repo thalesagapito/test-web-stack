484d1807db24a17113a85b91ccac65137b265819
Thales Agapito <thsousa101@gmail.com>
2021-11-27 17:15:30 -0300

    add user card avatar component

---
+++ src/components/users/UserCardAvatar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useUnsplashRandomImage } from '../../composables/unsplash'
+
+const props = withDefaults(
+  defineProps<{ preventFetch?: boolean }>(),
+  { preventFetch: false },
+)
+
+const { src, status } = useUnsplashRandomImage('profile', props.preventFetch)
+</script>
+
+<template>
+  <img v-if="status === 'success'" :src="src" alt="User avatar." class="user-card-avatar">
+  <div v-else class="user-card-avatar unavailable" aria-hidden="true" />
+</template>
+
+<style scoped lang="postcss">
+.user-card-avatar {
+  @apply w-40 h-40 rounded-full object-cover transition;
+  &.unavailable {
+    @apply bg-gray-100;
+  }
+}
+</style>
--- src/composables/unsplash.ts
+++ src/composables/unsplash.ts
@@ -6,17 +6,19 @@ import { Random } from 'unsplash-js/dist/methods/photos/types'
 const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY
 const unsplash = createApi({ accessKey })
 
-export function useUnsplashRandomImage(query: string) {
+export function useUnsplashRandomImage(query: string, preventFetch = false) {
   const src = ref<string>()
   const status = ref<'success' | 'loading' | 'error'>('loading')
 
-  unsplash.photos
-    .getRandom({ query })
-    .then(({ response }) => {
-      set(status, 'success')
-      set(src, (response as Random).urls.regular)
-    })
-    .catch(() => set(status, 'error'))
+  if (!preventFetch) {
+    unsplash.photos
+      .getRandom({ query })
+      .then(({ response }) => {
+        set(status, 'success')
+        set(src, (response as Random).urls.regular)
+      })
+      .catch(() => set(status, 'error'))
+  }
 
   return { src, status }
 }
