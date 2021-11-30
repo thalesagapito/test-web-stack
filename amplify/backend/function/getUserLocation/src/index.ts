1a79f4d17c5a6e13ce7f761f84dc571f1573a615
Thales Agapito <thsousa101@gmail.com>
2021-11-30 16:58:03 -0300

    add getUserLocation function

--- amplify/backend/function/getUserLocation/src/index.ts
+++ amplify/backend/function/getUserLocation/src/index.ts
@@ -1,12 +1,14 @@
+import fetch from 'node-fetch'
 import * as AWS from 'aws-sdk'
 import { APIGatewayProxyHandler } from 'aws-lambda'
 
+async function getUserInfoFromIpData(ip: string, apiKey: string) {
+  const response = await fetch(`https://api.ipdata.co/${ip}?api-key=${apiKey}`)
+  const userDataFromIp = await response.json()
+  return userDataFromIp
+}
+
 export const handler: APIGatewayProxyHandler = async(event) => {
-  // TODO implement
-  // console.log(event)
-
-  const userIp = event.requestContext.identity.sourceIp
-
   const { Parameters } = await (new AWS.SSM())
     .getParameters({
       Names: ['IP_DATA_API_KEY'].map(secretName => process.env[secretName]),
@@ -15,13 +17,16 @@ export const handler: APIGatewayProxyHandler = async(event) => {
     .promise()
   const ipDataApiKey = Parameters[0].Value as string
 
+  const userIp = event.requestContext.identity.sourceIp
+  const userDataFromIp = await getUserInfoFromIpData(userIp, ipDataApiKey)
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
     },
-    body: JSON.stringify(`Hello from Lambda! ${ipDataApiKey}, ${userIp}`),
+    body: JSON.stringify(userDataFromIp),
   }
   return response
 }
